bcd2c443b31836e8677e5dcd94169feaa4f576a1
Belthoise <belthoise.felix@gmail.com>
2025-12-31 19:16:25 +0100

    Added possibility to add, remove or get a component from another component

--- src/components/Component.cs
+++ src/components/Component.cs
@@ -6,5 +6,10 @@
         private EntityID m_EntityID = Entities.GetID(true);
 
         public abstract Component Clone();
+
+        public T Add<T>() where T : Component, new() => Components.GetOwner(this).Add<T>();
+        public T Add<T>(T component) where T : Component => Components.GetOwner(this).Add(component);
+        public void Remove<T>() where T : Component => Components.GetOwner(this).Remove<T>();
+        public T Get<T>() where T : Component => Components.GetOwner(this).Get<T>();
     }
 }
